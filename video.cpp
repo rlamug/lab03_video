06b7b10349c13f90d6313f66ff943c861249afaf
rlamug <rlamug@mail.csuchico.edu>
2018-02-10 22:01:11 -0800

    video.cpp

---
+++ video.cpp
@@ -0,0 +1,22 @@
+#include "video.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+Video::Video (string title, string url, string comment, float length, int rating){
+
+    m_title = title;
+    m_url = url;
+    m_comment = comment;
+    m_length = length;
+    m_rating = rating;
+}
+
+void Video::print(){
+    cout << m_title << ", " << m_url << ", " << m_comment << ", " << m_length << ", ";
+    for (int r = 1; r < m_rating; r ++){
+        cout << "*";
+    }
+    cout << endl;
+}
